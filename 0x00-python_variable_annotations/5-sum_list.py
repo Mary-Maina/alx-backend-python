303a1e4ed4b45a712719e6311ac5178b2ea16029
Mary-Maina <richellwanjiru@gmail.com>
2024-10-12 16:07:46 -0700

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module Documentation """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """func documentation"""
+    return sum(input_list)
